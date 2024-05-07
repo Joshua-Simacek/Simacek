b5e6b3547970a9e7032e8a26c5864c7b2768faec
Joshua Simacek <joshuasimacek@gmail.com>
2024-05-07 11:18:20 -0400

    Added DataTableExtensions

---
+++ Simacek/Data/DataTableExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System.Data;
+using System.Text;
+
+namespace Simacek.Data
+{
+    public static partial class DataTableExtensions
+    {
+        public static byte[] ToCsvBytes(this DataTable source)
+        {
+            var sb = new StringBuilder();
+
+            var tab = "";
+            foreach (DataColumn col in source.Columns)
+            {
+                sb.Append(tab + col.ColumnName.Replace(",", " "));
+                tab = ",";
+            }
+            sb.Append("\n");
+
+            var count = source.Columns.Count;
+            foreach (DataRow row in source.Rows)
+            {
+                tab = "";
+                for (int i = 0; i < count; i++)
+                {
+                    sb.Append(tab + row[i].ToString().Replace(",", " "));
+                    tab = ",";
+                }
+                sb.Append("\n");
+            }
+
+            return Encoding.Default.GetBytes(sb.ToString());
+        }
+    }
+}
