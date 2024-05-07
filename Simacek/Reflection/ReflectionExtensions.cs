c07ba98b035d1db87f8ec1604dc901b8bf3d5d3b
Joshua Simacek <joshuasimacek@gmail.com>
2024-05-07 12:05:47 -0400

    Added Reflection Extensions

---
+++ Simacek/Reflection/ReflectionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Simacek.Reflection
+{
+    public static partial class ReflectionExtensions
+    {
+        public static bool HasProperty(this Type model, string property)
+        {
+            return model.GetProperty(property) != null;
+        }
+    }
+}
