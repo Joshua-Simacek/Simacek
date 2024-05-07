f421b0294c9a29ee86fd7b695702a52f5ca57272
Joshua Simacek <joshuasimacek@gmail.com>
2024-05-07 11:18:19 -0400

    Added NameValueCollectionExtensions

---
+++ Simacek/Collections/NameValueCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+
+namespace Simacek.Collections
+{
+    public static partial class NameValueCollectionExtensions
+    {
+        public static IDictionary<string, object> ToDictionary(this NameValueCollection source)
+        {
+            return source.Keys.Cast<string>().ToDictionary<string, string, object>(key => key, key => source[key]);
+        }
+    }
+}
