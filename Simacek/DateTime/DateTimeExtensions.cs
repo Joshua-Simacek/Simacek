20434f0794c13858d4dc77449d16238fe26649a2
Joshua Simacek <joshuasimacek@gmail.com>
2024-05-07 11:22:02 -0400

    Added DateTimeExtensions

---
+++ Simacek/DateTime/DateTimeExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace Simacek.DateTime
+{
+    public static partial class DateTimeExtensions
+    {
+        //This logic is a direct translation of [dbo].[businessDaysAdd] from Sam's SQL Function to C#
+        public static System.DateTime AddBusinessDays(this System.DateTime source, int daysToAdd, params System.DateTime[] holidays)
+        {
+            var addsub = daysToAdd > 0 ? 1 : -1;
+            while (daysToAdd != 0)
+            {
+                source = source.AddDays(1.0 * addsub);
+                if (source.DayOfWeek != DayOfWeek.Saturday && source.DayOfWeek != DayOfWeek.Sunday && !holidays.Contains(source))
+                {
+                    daysToAdd -= (1 * addsub);
+                }
+            }
+
+            return source;
+        }
+    }
+}
