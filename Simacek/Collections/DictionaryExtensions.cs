d7cc431431b3f9ad3ee0f39e56bd703e3efe6048
Joshua Simacek <joshuasimacek@gmail.com>
2024-05-07 11:18:19 -0400

    Added DictionaryExtensions

---
+++ Simacek/Collections/DictionaryExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace Simacek.Collections
+{
+    public static partial class DictionaryExtensions
+    {
+        public static SqlParameter[] ToSqlParameters(this Dictionary<string, object> source)
+        {
+            return source.Select(param => new SqlParameter(param.Key, param.Value)).ToArray();
+        }
+
+    }
+}
